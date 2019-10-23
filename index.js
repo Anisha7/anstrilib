2d686be49cdfd18ab0f7ef5545e49014471ffc18
Anisha Jain <anishajain@outlook.com>
2019-10-22 17:11:02 -0700

    implemented string library functions and tests

--- index.js
+++ index.js
@@ -6,28 +6,63 @@ String.prototype.capitalize = function() {
  }
  
 // Capitalizes all letters of the string.
-String.prototype.allCaps() = function() {
+String.prototype.allCaps = function() {
     return this.toUpperCase()
 }
 
 // Capitalizes the first character of each word, in a space separated string
-String.prototype.capitalizeWords() = function() {
+String.prototype.capitalizeWords = function() {
     result = []
     this.split(" ").forEach(word => {
-        result.push(capitalize(word))
+        result.push(word.capitalize())
     });
     return result.join(" ")
 }
 
 // Capitalizes the first character of each headline word, in a space separated string
-// Headline words exclude the, in, a, an, and, but, for, at, by, from
-String.prototype.capitalizeHeadline() = function() {
+// Headline words exclude the, in, a, an, and, but, for, at, by, from (unless its the first word)
+String.prototype.capitalizeHeadline = function() {
     result = []
-    exceptions = new Set(["the", "in", "a", "an", "and", "but", "for", "at", "by", "from"])
+    exceptions = new Set(["the", "a", "an", "and", "but", "for", "at", "by", "from", "in"])
     this.split(" ").forEach(word => {
-        if (exceptions.has(word) === false) {
-            result.push(capitalize(word))
+        if (!exceptions.has(word)) {
+            result.push(word.capitalize())
+        } else {
+            result.push(word)
+        }
+    });
+    // Ensure the first word is always capitalized
+    return result.join(" ").capitalize()
+}
+
+// Removes all spaces from the beginning and end of a String along with any extra spaces in the middle
+String.prototype.removeExtraSpaces = function() {
+    result = []
+    this.trim().split(" ").forEach(word => {
+        if (word.length > 0) {
+            result.push(word.trim())
         }
     });
     return result.join(" ")
+}
+
+// Removes extra spaces and replaces spaces with the hyphen "-", and makes all characters lowercase.
+// " Hello world " -> "hello-world"
+String.prototype.kabobCase = function() {
+    return this.removeExtraSpaces().toLowerCase().split(" ").join("-")
+}
+
+// Removes extra space and replaces spaces with an underscore "_", and makes all characters lowercase.
+// " what the heck " -> "what_the_heck"
+String.prototype.snakeCase = function() {
+    return this.removeExtraSpaces().toLowerCase().split(" ").join("_")
+}
+
+// Lowercases the first character of the first word. 
+// Uppercases the first character of all other words, and removes all spaces.
+// Camel Case -> camelCase
+String.prototype.camelCase = function() {
+    let tempWordList = this.removeExtraSpaces().capitalizeWords().split(" ")
+    tempWordList[0] =  tempWordList[0].toLowerCase()
+    return tempWordList.join("")
 }
---
+++ test.js
@@ -0,0 +1,45 @@
+require('./index.js');
+
+test('capitalize', () => {
+    expect("hello".capitalize()).toBe("Hello")
+    expect("hello world".capitalize()).toBe("Hello world")
+    expect(" hello world".capitalize()).toBe(" hello world")
+})
+
+test('allCaps', () => {
+    expect("hello".allCaps()).toBe("HELLO")
+    expect("hello world".allCaps()).toBe("HELLO WORLD")
+    expect(" hello world".allCaps()).toBe(" HELLO WORLD")
+})
+
+test('capitalizeWords', () => {
+    expect("hello".capitalizeWords()).toBe("Hello")
+    expect("hello world".capitalizeWords()).toBe("Hello World")
+    expect(" hello world".capitalizeWords()).toBe(" Hello World")
+})
+
+test('capitalizeHeadline', () => {
+    expect("hello the a".capitalizeHeadline()).toBe("Hello the a")
+    expect("hello the from but boo".capitalizeHeadline()).toBe("Hello the from but Boo")
+    expect("hello world here in".capitalizeHeadline()).toBe("Hello World Here in")
+})
+
+test('removeExtraSpaces', () => {
+    expect("  hello the a  ".removeExtraSpaces()).toBe("hello the a")
+    expect("hello   the from   but boo".removeExtraSpaces()).toBe("hello the from but boo")
+})
+
+test('kabobCase', () => {
+    expect("hello the a".kabobCase()).toBe("hello-the-a")
+    expect(" hello the From but  Boo".kabobCase()).toBe("hello-the-from-but-boo")
+})
+
+test('snakeCase', () => {
+    expect("hello the a".snakeCase()).toBe("hello_the_a")
+    expect(" hello the From but  Boo".snakeCase()).toBe("hello_the_from_but_boo")
+})
+
+test('camelCase', () => {
+    expect("hello the a".camelCase()).toBe("helloTheA")
+    expect(" hello the From but  Boo".camelCase()).toBe("helloTheFromButBoo")
+})
