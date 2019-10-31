be3d037339b1c26751738eebb33e96109011f15e
Anisha Jain <anishajain@outlook.com>
2019-10-31 14:11:04 -0700

    yaml added

--- index.js
+++ index.js
@@ -2,13 +2,17 @@
 
 // Capitalizes the first letter of the string.
 // TODO: If first letter is a space, this won't capitalize the next one.
-String.prototype.capitalize = () => this.charAt(0).toUpperCase() + this.substr(1);
+String.prototype.capitalize = function () {
+  return this.charAt(0).toUpperCase() + this.substr(1);
+};
 
 // Capitalizes all letters of the string.
-String.prototype.allCaps = () => this.toUpperCase();
+String.prototype.allCaps = function () {
+  return this.toUpperCase();
+};
 
 // Capitalizes the first character of each word, in a space separated string
-String.prototype.capitalizeWords = () => {
+String.prototype.capitalizeWords = function () {
   const result = [];
   this.split(' ').forEach((word) => {
     result.push(word.capitalize());
@@ -18,7 +22,7 @@ String.prototype.capitalizeWords = () => {
 
 // Capitalizes the first character of each headline word, in a space separated string
 // Headline words exclude the, in, a, an, and, but, for, at, by, from (unless its the first word)
-String.prototype.capitalizeHeadline = () => {
+String.prototype.capitalizeHeadline = function () {
   const result = [];
   const exceptions = new Set(['the', 'a', 'an', 'and', 'but', 'for', 'at', 'by', 'from', 'in']);
   this.split(' ').forEach((word) => {
@@ -33,7 +37,7 @@ String.prototype.capitalizeHeadline = () => {
 };
 
 // Removes all spaces from the beginning and end of a string along with any extra spaces in the middle
-String.prototype.removeExtraSpaces = () => {
+String.prototype.removeExtraSpaces = function () {
   const result = [];
   this.trim().split(' ').forEach((word) => {
     if (word.length > 0) {
@@ -45,17 +49,21 @@ String.prototype.removeExtraSpaces = () => {
 
 // Removes extra spaces and replaces spaces with the hyphen "-", and makes all characters lowercase.
 // " Hello world " -> "hello-world"
-String.prototype.kabobCase = () => this.removeExtraSpaces().toLowerCase().split(' ').join('-');
+String.prototype.kabobCase = function () {
+  return this.removeExtraSpaces().toLowerCase().split(' ').join('-');
+};
 
-// Removes extra space and replaces spaces with an underscore "_", 
+// Removes extra space and replaces spaces with an underscore "_",
 // and makes all characters lowercase.
 // " what the heck " -> "what_the_heck"
-String.prototype.snakeCase = () => this.removeExtraSpaces().toLowerCase().split(' ').join('_');
+String.prototype.snakeCase = function () {
+  return this.removeExtraSpaces().toLowerCase().split(' ').join('_');
+};
 
 // Lowercases the first character of the first word.
 // Uppercases the first character of all other words, and removes all spaces.
 // Camel Case -> camelCase
-String.prototype.camelCase = () => {
+String.prototype.camelCase = function () {
   const tempWordList = this.removeExtraSpaces().capitalizeWords().split(' ');
   tempWordList[0] = tempWordList[0].toLowerCase();
   return tempWordList.join('');
